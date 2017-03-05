dc06838a32aa3cc041e7dd0c5e3efcf41467466b
Jakub Stefanski <code@jakubstefanski.com>
2017-03-05 10:15:41 +0100

    Add Arch Linux configure script

---
+++ archlinux/configure.sh
@@ -0,0 +1,64 @@
+# T430 laptop
+#pacman -S xf86-video-intel
+#pacman -S xf86-input-synaptics
+
+# system utils
+pacman -S sudo
+pacman -S bash-completion
+pacman -S syslinux
+pacman -S dosfstools
+pacman -S mlocate
+pacman -S htop
+pacman -S atop
+pacman -S iftop
+
+# security
+pacman -S gnupg
+pacman -S pinentry
+pacman -S pinentry-curses
+pacman -S openssh
+
+# dev utils
+pacman -S vim
+pacman -S git
+pacman -S wget
+pacman -S ranger
+
+# fonts
+pacman -S ttf-dejavu
+pacman -S ttf-ionicons
+
+# visual
+pacman -S xorg
+pacman -S mesa
+pacman -S i3-wm i3lock i3status
+pacman -S dmenu
+pacman -S feh
+pacman -S dunst
+pacman -S gtk-engine-murrine
+pacman -S lxappearance
+pacman -S gnome-themes-standard
+pacman -S arc-gtk-theme
+pacman -S arc-icon-theme
+
+# audio
+pacman -S alsa-utils
+
+# mail
+pacman -S isync
+pacman -S mutt
+
+# network
+pacman -S openvpn
+pacman -S networkmanager
+pacman -S networkmanager-openvpn
+
+# applications
+pacman -S gvim
+pacman -S elinks
+pacman -S rxvt-unicode
+pacman -S mplayer
+pacman -S keepass
+pacman -S arandr
+pacman -S pcmanfm
+pacman -S virtualbox
