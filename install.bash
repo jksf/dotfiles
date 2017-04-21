ad749a2b83bae385836e4580e8dc292e7e280154
Jakub Stefanski <code@jakubstefanski.com>
2017-04-21 15:22:52 +0200

    Use ~/.local/bin instead of ~/bin

--- install.bash
+++ install.bash
@@ -30,52 +30,52 @@ link_prompt() {
 }
 
 # git
-link_prompt $DIR/git/gitconfig $HOME/.gitconfig
+link_prompt "$DIR/git/gitconfig" "$HOME/.gitconfig"
 
 # vim
-link_prompt $DIR/vim/vimrc $HOME/.vimrc
-link_prompt $DIR/vim/gvimrc $HOME/.gvimrc
-link_prompt $DIR/vim $HOME/.vim
+link_prompt "$DIR/vim/vimrc" "$HOME/.vimrc"
+link_prompt "$DIR/vim/gvimrc" "$HOME/.gvimrc"
+link_prompt "$DIR/vim" "$HOME/.vim"
 
 # bash
-link_prompt $DIR/bash/bashrc $HOME/.bashrc
-link_prompt $DIR/bash/bash_profile $HOME/.bash_profile
-link_prompt $DIR/bash/inputrc $HOME/.inputrc
-link_prompt $DIR/bash/git-completion.bash $HOME/.git-completion.bash
+link_prompt "$DIR/bash/bashrc" "$HOME/.bashrc"
+link_prompt "$DIR/bash/bash_profile" "$HOME/.bash_profile"
+link_prompt "$DIR/bash/inputrc" "$HOME/.inputrc"
+link_prompt "$DIR/bash/git-completion.bash" "$HOME/.git-completion.bash"
 
 # windows
 if [[ "$(uname)" == MINGW* ]]; then
-    link_prompt $DIR/mintty/minttyrc $HOME/.minttyrc
+    link_prompt "$DIR/mintty/minttyrc" "$HOME/.minttyrc"
 else
     # screen
-    link_prompt $DIR/screen/screenrc $HOME/.screenrc
+    link_prompt "$DIR/screen/screenrc" "$HOME/.screenrc"
 
     # X
-    link_prompt $DIR/x/Xdefaults $HOME/.Xdefaults
-    link_prompt $DIR/x/xsessionrc $HOME/.xsessionrc
+    link_prompt "$DIR/x/Xdefaults" "$HOME/.Xdefaults"
+    link_prompt "$DIR/x/xsessionrc" "$HOME/.xsessionrc"
 
     # X Scripts
-    mkdir -p $HOME/bin
-    link_prompt $DIR/x/scripts/pixlock $HOME/bin/pixlock
-    link_prompt $DIR/x/scripts/monitorlayout $HOME/bin/monitorlayout
-    link_prompt $DIR/x/scripts/ranger-open $HOME/bin/ranger-open
+    mkdir -p "$HOME/.local/bin"
+    link_prompt "$DIR/x/scripts/pixlock" "$HOME/.local/bin/pixlock"
+    link_prompt "$DIR/x/scripts/monitorlayout" "$HOME/.local/bin/monitorlayout"
+    link_prompt "$DIR/x/scripts/ranger-open" "$HOME/.local/bin/ranger-open"
 
     # Images
-    mkdir -p $HOME/Images
-    link_prompt $DIR/x/images/lock.png $HOME/Images/lock.png
+    mkdir -p "$HOME/Images"
+    link_prompt "$DIR/x/images/lock.png" "$HOME/Images/lock.png"
 
     # i3
-    mkdir -p $HOME/.config/i3
-    link_prompt $DIR/x/i3/config $HOME/.config/i3/config
-    link_prompt $DIR/x/i3/i3status.conf $HOME/.config/i3/i3status.conf
+    mkdir -p "$HOME/.config/i3"
+    link_prompt "$DIR/x/i3/config" "$HOME/.config/i3/config"
+    link_prompt "$DIR/x/i3/i3status.conf" "$HOME/.config/i3/i3status.conf"
 
     # dunst
-    mkdir -p $HOME/.config/dunst
-    link_prompt $DIR/x/dunst/dunstrc $HOME/.config/dunst/dunstrc
+    mkdir -p "$HOME/.config/dunst"
+    link_prompt "$DIR/x/dunst/dunstrc" "$HOME/.config/dunst/dunstrc"
 
     # gtk
-    link_prompt $DIR/x/gtk/gtkrc-2.0 $HOME/.gtkrc-2.0
-    link_prompt $DIR/x/gtk/gtkrc-3.0 $HOME/.config/gtk-3.0/settings.ini
+    link_prompt "$DIR/x/gtk/gtkrc-2.0" "$HOME/.gtkrc-2.0"
+    link_prompt "$DIR/x/gtk/gtkrc-3.0" "$HOME/.config/gtk-3.0/settings.ini"
 fi
 
 if [ -n "$NEW" ]; then
